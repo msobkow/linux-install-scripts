a6572bad0d13dc524a7b5e7d973be018ca07fa7c
Mark Stephen Sobkow <msobkow@sasktel.net>
2020-09-07 19:26:39 +0000

    Working on my setup scripts.

--- wsl-ubuntu-18_04-lts-amd64/Install-WSL-Ubuntu-18_04-lts-amd64-02-gui.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt-get install xscreensaver xscreensaver-gl-extra firefox fonts-lyx gtk3-engines-xfce xscreensaver xscreensaver-data xscreensaver-gl xscreensaver-gl-extra xserver-common 
--- wsl-ubuntu-18_04-lts-amd64/Install-WSL-Ubuntu-18_04-lts-amd64-05-mysql.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt-get install odbcinst php-odbc python3-pyodbc python3-pyodbc-dbg unixodbc unixodbc-dev mysql-client mysql-common mysql-server mysql-testsuite libmysql++-dev libmysql++-doc
