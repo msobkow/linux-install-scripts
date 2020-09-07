3f3e8d141600ddc62dd6436677736d67a7021c1c
Mark Stephen Sobkow <msobkow@sasktel.net>
2020-09-07 19:36:07 +0000

    Merge the changes made for installing Ian's VM with the changes I've made to support my other VMs.  Between the two, Ubuntu 18.04 LTS amd64 boxen should be happy now.

--- ubuntu-18_04-lts-amd64/Install-Ubuntu-18_04-lts-amd64-00-bind9.bash
+++ ubuntu-18_04-lts-amd64/Install-Ubuntu-18_04-lts-amd64-00-bind9.bash
@@ -1,6 +1,18 @@
 #!/bin/bash
-<<<<<<< HEAD
-sudo apt-get install bind9 dnsutils
-=======
+#	linux-install-scripts
+#
+#	Copyright 2020 Mark Stephen Sobkow msobkow@sasktel.net
+#
+#		Licensed under the Apache License, Version 2.0 (the "License");
+#		you may not use this file except in compliance with the License.
+#		You may obtain a copy of the License at
+#
+#		http://www.apache.org/licenses/LICENSE-2.0
+#
+#		Unless required by applicable law or agreed to in writing, software
+#		distributed under the License is distributed on an "AS IS" BASIS,
+#		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#		See the License for the specific language governing permissions and
+#		limitations under the License.
+#
 sudo apt-get install bind9
->>>>>>> 64aa99d83fa729cc7c095c0165ed0fe10c5ec9dd
