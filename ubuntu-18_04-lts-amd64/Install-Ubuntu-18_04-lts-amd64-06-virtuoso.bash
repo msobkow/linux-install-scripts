4c73fd16b8d1694ba275896a8f43faa18d41533b
Mark Stephen Sobkow <msobkow@sasktel.net>
2020-09-08 19:02:22 +0000

    Automate the download and installation of a specific release of Virtuoso Opensource pre-built binaries.

--- ubuntu-18_04-lts-amd64/Install-Ubuntu-18_04-lts-amd64-06-virtuoso.bash
+++ ubuntu-18_04-lts-amd64/Install-Ubuntu-18_04-lts-amd64-06-virtuoso.bash
@@ -15,5 +15,59 @@
 #		See the License for the specific language governing permissions and
 #		limitations under the License.
 #
+
+export forceInstall="$*"
+
+# If the virtuoso user doesn't exist, create it
+
+VIRTUOSO_USER_EXISTS=`grep '^virtuoso:' /etc/passwd`
+if [ "$VIRTUOSO_USER_EXISTS" == "" ]; then
+	sudo adduser virtuoso
+fi
+
+# Install prerequisites
 sudo apt-get install autoconf automake libtool flex bison gperf gawk m4 make openssl dh-make bzr-builddeb
+
+# Make sure the Downloads directory exists
+if [ ! -d $HOME/Downloads ]; then
+	mkdir $HOME/Downloads
+fi
+
+# If "force" was specified, delete any existing download
+
+if [ "$forceInstall" == "force" ]; then
+	echo "Forcing redownload"
+	rm -f $HOME/Downloads/virtuoso-opensource.x86_64-generic_glibc25-linux-gnu.tar.gz
+fi
+
+# 
+if [ ! -f $HOME/Downloads/virtuoso-opensource.x86_64-generic_glibc25-linux-gnu.tar.gz ]; then
+	pushd $HOME/Downloads
+	rm -f download
+	wget https://sourceforge.net/projects/virtuoso/files/virtuoso/7.2.5/virtuoso-opensource.x86_64-generic_glibc25-linux-gnu.tar.gz/download
+	if [ -f download ]; then
+		mv download virtuoso-opensource.x86_64-generic_glibc25-linux-gnu.tar.gz
+	fi
+	popd
+fi
+if [ -f $HOME/Downloads/virtuoso-opensource.x86_64-generic_glibc25-linux-gnu.tar.gz ]; then
+	if [ "$forceInstall" == "force" ]; then
+		echo "Removing any existing installation of Virtuoso 7.2.5 ..."
+		sudo rm -Rf /opt/virtuoso/7.2.5
+	fi
+	if [ ! -d /opt/virtuoso ]; then
+		sudo mkdir /opt/virtuoso
+		sudo chown virtuoso.virtuoso /opt/virtuoso
+		sudo chmod 755 /opt/virtuoso
+	fi
+	if [ ! -d /opt/virtuoso/7.2.5 ]; then
+		sudo mkdir /opt/virtuoso/7.2.5
+		sudo chown virtuoso.virtuoso /opt/virtuoso/7.2.5
+		sudo chmod 755 /opt/virtuoso/7.2.5
+	fi
+	pushd /opt/virtuoso/7.2.5
+	sudo tar xvfz $HOME/Downloads/virtuoso-opensource.x86_64-generic_glibc25-linux-gnu.tar.gz
+	popd
+	sudo chown -R virtuoso.virtuoso /opt/virtuoso/7.2.5
+fi
 
