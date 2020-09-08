00e8c0a211bc1c6adac7abbcc82cf310beaff2f3
Mark Stephen Sobkow <msobkow@sasktel.net>
2020-09-08 02:30:00 +0000

    Separate the bind9 installation, as workstations shouldn't install it.

---
+++ ubuntu-20_04-lts-amd64/Install-Ubuntu-20_04-lts-amd64-06-virtuoso.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+#	linux-install-scripts
+#
+#	Packages required to build and install virtuoso-opensource
+#
+sudo apt-get install autoconf automake libtool flex bison gperf gawk m4 make openssl dh-make bzr-builddeb
+
