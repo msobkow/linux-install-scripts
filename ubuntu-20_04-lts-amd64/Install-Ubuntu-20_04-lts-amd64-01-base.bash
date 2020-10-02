23128f3fdb63bcd9d54085f86e1c127505377ecd
Mark Stephen Sobkow <msobkow@sasktel.net>
2020-10-02 04:09:23 +0000

    No such package, eh, doh! :)

--- ubuntu-20_04-lts-amd64/Install-Ubuntu-20_04-lts-amd64-01-base.bash
+++ ubuntu-20_04-lts-amd64/Install-Ubuntu-20_04-lts-amd64-01-base.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get install git aptitude ssh zip unzip bzip2 gzip unrar lzip lrzip p7zip-rar rar dpkg apt libc-bin bash findutils gnupg2 gnupg-agent vim-common manpages texlive-binaries docbook-xsl-doc-html docbook-xsl-doc-pdf docbook-xsl-doc-text docbook-xsl-saxon aspell-doc spellutils exfat-utils f2fs-tools npm setserial ir-keytable cifs-utils tcl-tclreadline psutils sysstat iostat memstat samba smbclient ssh-askpass csh dnsutils
+sudo apt-get install git aptitude ssh zip unzip bzip2 gzip unrar lzip lrzip p7zip-rar rar dpkg apt libc-bin bash findutils gnupg2 gnupg-agent vim-common manpages texlive-binaries docbook-xsl-doc-html docbook-xsl-doc-pdf docbook-xsl-doc-text docbook-xsl-saxon aspell-doc spellutils exfat-utils f2fs-tools npm setserial ir-keytable cifs-utils tcl-tclreadline psutils sysstat memstat samba smbclient ssh-askpass csh dnsutils
 
