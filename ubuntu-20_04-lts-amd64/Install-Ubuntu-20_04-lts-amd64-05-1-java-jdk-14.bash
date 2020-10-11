275dd878724a8cdd761716dbf3228fa0d829adc1
Mark Stephen Sobkow <msobkow@sasktel.net>
2020-10-11 01:53:24 +0000

    The JDK 14 /etc/environment setup needs to auto-correct the bad entries I have on the other hosts, so I've made it much smarter than it used to be about what and how it edits the /etc/environment file.

--- ubuntu-20_04-lts-amd64/Install-Ubuntu-20_04-lts-amd64-05-1-java-jdk-14.bash
+++ ubuntu-20_04-lts-amd64/Install-Ubuntu-20_04-lts-amd64-05-1-java-jdk-14.bash
@@ -17,7 +17,8 @@
 #
 sudo apt-get install openjdk-14-jdk
 sudo update-alternatives --config java
-ENV_VAR_FOUND=`grep JSEP /etc/environment`
+CHANGE_PROMPTED="n"
+ENV_VAR_FOUND=`grep "JSEP=" /etc/environment`
 if [ "$ENV_VAR_FOUND" == "" ]; then
 	echo '$' >add-to-env.ex
 	echo 'a' >>add-to-env.ex
@@ -26,12 +27,44 @@ if [ "$ENV_VAR_FOUND" == "" ]; then
 	echo 'JAVA_HOME="/usr/lib/jvm/java-14-openjdk-amd64"' >>add-to-env.ex
 	echo 'JDK_HOME="/usr/lib/jvm/java-14-openjdk-amd64"' >>add-to-env.ex
 	echo 'JAVAC="javac"' >>add-to-env.ex
-	echo 'JAVACMD="/usr/lib/jvm/java-14-openjdk-amd64/java"' >>add-to-env.ex
+	echo 'JAVACMD="/usr/lib/jvm/java-14-openjdk-amd64/bin/java"' >>add-to-env.ex
 	echo '.' >>add-to-env.ex
 	echo 'w' >>add-to-env.ex
 	echo 'q' >>add-to-env.ex
 	echo '' >>add-to-env.ex
 	sudo ex /etc/environment <add-to-env.ex
 	rm -f add-to-env.ex 
+	if [ "$CHANGE_PROMPTED" != "y" ]; then
+		echo "WARNING: /etc/environment has been changed, you'll need to log out and back in again for the changes to take effect" && CHANGE_PROMPTED="y"
+	fi
 fi
 
+ENV_PATH_CONTAINS_JDK_HOME_BIN=`grep "PATH=" /etc/environment | grep "/usr/lib/jvm/java-14-openjdk-amd64/bin"`
+if [ "$ENV_PATH_CONTAINS_JDK_HOME_BIN" == "" ]; then
+	echo '1,$s,^PATH=.*$,PATH="/usr/local/sbin:/usr/local/bin:/usr/lib/jvm/java-14-openjdk-amd64/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin",g' >update-env-path.ex
+	echo 'w' >>update-env-path.ex
+	echo 'q' >>update-env-path.ex
+	echo '' >>update-env-path.ex
+	sudo ex /etc/environment <update-env-path.ex
+	rm -f update-env-path.ex
+	if [ "$CHANGE_PROMPTED" != "y" ]; then
+		echo "WARNING: /etc/environment has been changed, you'll need to log out and back in again for the changes to take effect" && CHANGE_PROMPTED="y"
+	fi
+fi
+unset ENV_PATH_CONTAINS_JDK_HOME_BIN
+
+ENV_JAVACMD_IS_CORRECT=`grep "JAVACMD=" /etc/environment | grep "/usr/lib/jvm/java-14-openjdk-amd64/bin/java"`
+if [ "$ENV_JAVACMD_IS_CORRECT" == "" ]; then
+	echo '1,$s,^JAVACMD=.*$,JAVACMD="/usr/lib/jvm/java-14-openjdk-amd64/bin/java",' >update-env-javacmd.ex
+	echo 'w' >>update-env-javacmd.ex
+	echo 'q' >>update-env-javacmd.ex
+	echo '' >>update-env-javacmd.ex
+	sudo ex /etc/environment <update-env-javacmd.ex
+	rm -f update-env-javacmd.ex
+	if [ "$CHANGE_PROMPTED" != "y" ]; then
+		echo "WARNING: /etc/environment has been changed, you'll need to log out and back in again for the changes to take effect" && CHANGE_PROMPTED="y"
+	fi
+fi
+unset ENV_JAVACMD_IS_CORRECT
+
+unset CHANGE_PROMPTED
