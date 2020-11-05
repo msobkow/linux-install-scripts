79724d64a328cbfe674d28e643a4ac53159acab9
Mark Stephen Sobkow <msobkow@sasktel.net>
2020-11-05 05:09:32 -0600

    Add and use scripts to install x11vnc and gvncviewer for remote desktops.

--- ubuntu-20_04-lts-amd64/host-all-ubuntu-20_04-lts-amd64-initial-packages.bash
+++ ubuntu-20_04-lts-amd64/host-all-ubuntu-20_04-lts-amd64-initial-packages.bash
@@ -2,3 +2,4 @@
 # Initial package install stack for all hosts at dev.msscf.org
 ./Install-Ubuntu-20_04-lts-amd64-01-base.bash
 ./Install-Ubuntu-20_04-lts-amd64-02-gui.bash
+./Install-Ubuntu-20_04-lts-amd64-18-x11vncremoting.bash
