c8b7d10841ba60b80af46e827753ad40810d584b
Mark Stephen Sobkow <msobkow@sasktel.net>
2021-01-23 04:00:01 -0600

    Purge 18.04; it is dead as of 2021Q1. :)

--- ubuntu-18_04-lts-amd64-UNSUPPORTED/purge-mqtt.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt-get purge node-mqtt-packet python-paho-mqtt python3-hbmqtt python3-paho-mqtt
--- ubuntu-18_04-lts-amd64-UNSUPPORTED/purge-virtuoso.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt-get purge virtuoso-opensource virtuoso-opensource-6.1 virtuoso-opensource-6.1-bin virtuoso-opensource-6.1-common virtuoso-server virtuoso-vad-bpel virtuoso-vad-conductor virtuoso-vad-demo virtuoso-vad-doc virtuoso-vad-isparql virtuoso-vad-ods virtuoso-vad-rdfmappers virtuoso-vad-sparqldemo virtuoso-vad-syncml virtuoso-vad-tutorial virtuoso-vsp-startpage
